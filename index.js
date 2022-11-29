c11f2bc80a767d6549eca5c731320edc10b21226
joyshilnil <charticchandra@gmail.com>
2022-11-29 21:10:50 +0600

    server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000
@@ -19,24 +19,50 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
   try {
-    const categorysCollecation = client.db('laptop').collection('category')
-    const usersCollecation = client.db('laptop').collection('user')
+    const categorysCollecation = client.db('laptop').collection('categorys')   
+    const categoryProductsCollecation = client.db('laptop').collection('categoryproducts')
 
-    app.put('/user/:email', async (req, res) => {
-      const email = req.params.email
-      const user = req.body
-      const filter = { email: email }
-      const options = { upsert: true}
-      const updateDoc = {
-        $set: user,
-      }
-      const result = await usersCollecation.updateOne(filter, updateDoc, options)
-      console.log(result)
+    app.get('/categorys', async (req, res) => {
+      const query = {};
+      const categorys = await categorysCollecation.find(query).toArray();
+      res.send(categorys);
+  });
 
-      const token = jwt.sign(user, process.env.TOKEN, {
-        expiresIn: '1d',
-      })
-    })
+
+  app.get('/categorys/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { id: id };
+    const category = await categoryProductsCollecation.find(query).toArray();
+    res.send(category);
+})
+
+
+
+
+
+
+
+
+
+
+
+    // const usersCollecation = client.db('laptop').collection('user')
+
+    // app.put('/user/:email', async (req, res) => {
+    //   const email = req.params.email
+    //   const user = req.body
+    //   const filter = { email: email }
+    //   const options = { upsert: true}
+    //   const updateDoc = {
+    //     $set: user,
+    //   }
+    //   const result = await usersCollecation.updateOne(filter, updateDoc, options)
+    //   console.log(result)
+
+    //   const token = jwt.sign(user, process.env.TOKEN, {
+    //     expiresIn: '1d',
+    //   })
+    // })
 
 
     console.log('Database Connected...')
